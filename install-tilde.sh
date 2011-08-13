f47e8f0d1107d3667aabde4fab7c97486cc0d23c
Ryan Sorensen <rcsorensen@gmail.com>
2011-08-13 13:27:22 -0700

    Created bootstrap script

---
+++ install-tilde.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+TILDE_DIR="$HOME/.tilde"
+if ! which git > /dev/null; then
+	echo "Git's required to bootstrap tilde"
+	exit 2
+fi
+
+if ! git clone "git@github.com:rcs/tilde.git" "$TILDE_DIR"; then
+	git clone "git://github.com/rcs/tilde.git" "$TILDE_DIR"
+fi
+
+#$TILDE_DIR/bin/tilde init
+
+
+echo "Tilde initialized. Add $TILDE_DIR/bin to your path."
+echo "($ echo 'export PATH=\"\$HOME/.tilde/bin:\$PATH\"' >> .bash_profile )"
+
